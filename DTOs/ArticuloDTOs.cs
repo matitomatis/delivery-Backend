1575d9d1bea1a5c6616f36a8104b6bb4141558ee
Matias <mati2003tomatis@gmail.com>
2026-08-26 16:25:28 -0300

    Implementacion de DTOs para Articulos

---
+++ DTOs/ArticuloDTOs.cs
@@ -0,0 +1,19 @@
+﻿namespace delivery.DTOs
+{
+    // El formato que devolvemos a la pantalla (con el ID)
+    public class ArticuloGetDTO
+    {
+        public int CodArticulo { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public short Stock { get; set; }
+    }
+
+    // El formato que le pedimos al usuario para crear uno nuevo (¡sin el ID!)
+    public class ArticuloCreateDTO
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public short Stock { get; set; }
+    }
+}
